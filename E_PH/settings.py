c0ac66d5b9bd481baa677f1c6288911e0518f543
Abdul Hadi Mazbah <ahmazbah6@gmail.com>
2022-05-25 12:17:01 +0800

    update settings.py

--- E_PH/settings.py
+++ E_PH/settings.py
@@ -138,5 +138,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'thor1899son@gmail.com'
-EMAIL_HOST_PASSWORD = '01139313428g.'
+EMAIL_HOST_USER = '*********************'
+EMAIL_HOST_PASSWORD = '************'
